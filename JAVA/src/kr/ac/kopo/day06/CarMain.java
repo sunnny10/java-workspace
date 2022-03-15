8e41925515e466c611bdde94a9ae91539af5b953
Sun Jung <wjdtjs905@likelion.org>
2022-03-15 17:20:13 +0900

    0315 class

--- JAVA/src/kr/ac/kopo/day06/CarMain.java
+++ JAVA/src/kr/ac/kopo/day06/CarMain.java
@@ -3,18 +3,27 @@ package kr.ac.kopo.day06;
 //프로그램 = 하나의 실행클래스 + 여러개의 참조클래스
 // 실행클래스
 public class CarMain {
-
+	//아직까지는 객체지향 아님.
+	// 객제지향스럽게 만든다 : 인터페이스까지는 배워야... 
 	public static void main(String[] args) {
+		
+		String[] names = new String[2];
+		String[] company = new String[2];
+		
+		names[0] = "소나타";
+		company[0] = "현대";
 		
 		// new 클래스명()
 		// heap 영역에 생성. 이름없음
 		// 괄호가 생성자다.
 		Car c = new Car();
 		c.name = "소나타";
+		c.company = "현대";
 		// new 써서 문자열 생성해도 된다.                                                                     
 		
 		Car c2 = new Car();
 		c2.name = "K9";
+		c2.company = "기아";
 		
 		System.out.println("첫번째 자동차명 : " + c.name);
 		System.out.println("두번째 자동차명 : " + c2.name);
---
+++ JAVA/src/kr/ac/kopo/day06/HandphoneMain.java
@@ -0,0 +1,52 @@
+package kr.ac.kopo.day06;
+
+// 추상화시켜서 클래스로 설계시킨다.
+
+//참조클래스
+//여기서는 public 붙이면 안된다. 클래스는 따로 파일로 만들어주는 게 좋다!!!
+class Handphone {
+
+	String name;		// 실사용자명
+	String phoneNumber; // 핸드폰번호
+	String company;		// 제조사
+
+}
+
+public class HandphoneMain {
+	
+
+	public static void main(String[] args) {
+		
+		Handphone hp = new Handphone();
+		hp.name = "홍길동";
+		hp.phoneNumber = "010-1111-2222";
+		hp.company = "삼성";
+		
+		Handphone hp2 = new Handphone();
+		hp2.name = "강길동";
+		hp2.phoneNumber = "010-3333-4444";
+		hp2.company = "애플";
+		
+		Handphone hp3 = new Handphone();
+		hp3.name = "윤길동";
+		hp3.phoneNumber = "010-5555-6666";
+		hp3.company = "화웨이";
+		
+		//Handphone 객체를 3개 만들고 있는 객체를 만들 수 있지 않을까...
+		Handphone[] hpArr = {hp, hp2, hp3};
+		
+		for(Handphone phone : hpArr) {
+			System.out.println("사용자 : " + phone.name + ", 전화번호 : " + phone.phoneNumber + ", 제조사 : " + phone.company); 
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
